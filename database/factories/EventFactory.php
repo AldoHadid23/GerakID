5a0df15ef11132cdadad9c1ec560320614f8f260
AldoHadid23 <aldohadid23@gmail.com>
2024-02-03 08:49:55 +0700

    Mengubah kapital pilihan type

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -25,7 +25,7 @@ class EventFactory extends Factory
             // 'body' => '<p>' . implode('</p><p>', $this->faker->paragraphs(mt_rand(5,10))) . '</p>',
             'body' => collect($this->faker->paragraphs(mt_rand(5,10)))->map(fn($p) => "<p>$p</p>")->implode(''),
             'price' => $this->faker->randomNumber(5, true),
-            'type' => $this->faker->randomElement(['offline', 'online', 'hybrid']),
+            'type' => $this->faker->randomElement(['Offline', 'Online', 'Hybrid']),
             'quota' => $this->faker->numberBetween(0, 100),
             'registration_start' => $this->faker->dateTime(),
             'registration_end' => $this->faker->dateTime(),
